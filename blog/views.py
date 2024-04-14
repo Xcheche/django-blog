da6c9bd99c54a868f7ffcf03934230da68c04a9e
xcheche <chekwubechukwu23@gmail.com>
2024-04-14 12:51:13 +0100

    done

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # blog/views.py
-
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
 from django.shortcuts import render
 from blog.models import Post, Comment
 
@@ -24,11 +25,36 @@ def blog_category(request, category):
 
 
 def blog_detail(request, pk):
+    # Retrieve the post object
     post = Post.objects.get(pk=pk)
+    
+    # Instantiate an empty comment form
+    form = CommentForm()
+    
+    # If the request method is POST, process the form submission
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # Create a new comment associated with the post
+            comment = Comment(
+                author=form.cleaned_data["author"],
+               
+                body=form.cleaned_data["body"],
+                post=post,  # Use the retrieved post object here
+            )
+            comment.save()
+            # Redirect back to the current page after successful form submission
+            return HttpResponseRedirect(request.path_info)
+
+    # Retrieve all comments associated with the post
     comments = Comment.objects.filter(post=post)
+    
+    # Prepare the context to pass to the template
     context = {
         "post": post,
         "comments": comments,
+        "form": form,  # Use the instantiated form here
     }
 
+    # Render the template with the provided context
     return render(request, "blog/detail.html", context)
